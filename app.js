dee64af017e186ff6676ee7d9766c52d414b53a5
Sahira100 <e19100@eng.pdn.ac.lk>
2024-08-12 13:30:50 +0530

    error handeling

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 const express = require("express");
+const notFound = require("./middleware/not-found");
+const erroHandler = require("./middleware/error-handler");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 
 const taskRouter = require("./routes/tasks");
-const port = 3000;
 
 const app = express();
 
 app.use(express.json());
-
-//routes
 app.use(express.static("./public"));
 
-app.use("/api/v1/task", taskRouter);
+//routes
+app.use("/api/v1/tasks", taskRouter);
+app.use("*", notFound);
+
+app.use(erroHandler);
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
 
-    app.listen(port, () => {
-      console.log("server is listening on port:" + port);
+    app.listen(process.env.PORT, () => {
+      console.log("server is listening on port:" + process.env.PORT);
     });
   } catch (err) {
     console.log("Server ❌");
